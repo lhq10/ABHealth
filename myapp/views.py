b0320aacf17cd0d991241db5fb7e12c8be4279ce
lhq10 <k38chxhlequang@gmail.com>
2024-09-26 13:29:24 +0700

    Initial commit

---
+++ myapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def service(request):
+    return render(request, 'service.html')
+
+def appointment(request):
+    return render(request, 'appointment.html')
+
+
